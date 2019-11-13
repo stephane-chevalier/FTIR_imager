f1dd198d0984fdac8f73dc005591fbfea94abdbe
Stéphane Chevalier <stephane.chevalier@u-bordeaux.fr>
2019-11-13 12:22:17 +0100

    MAJ du programme de FFT
    Remplacement de la double boucle par un reshape et un simple boucle pour gagner en temps de calcul.
    Supprimer les commentaires de progression de la fft.

--- I2S.m
+++ I2S.m
@@ -13,32 +13,28 @@ function [S,nub,inter_apo] = I2S(inter,l,res,a)
     sizes = size(inter); %taille de la matrice    
     Ni = sizes(2);
     Nj = sizes(1);
+    Nk = sizes(3);
     
     % coef apodization
-    sigma = a*sizes(3)/4113;
+    sigma = a*Nk/4113; %4113 est une valeur de référence pour res = 4 cm-1
     
     % distance between two frames
-    dl = l(2)-l(1);
-    
-    for i = 1:Ni
-        if ~isempty(find(i == 1:round(Ni/9):Ni))
-            disp(['Calcul fait.... à ',num2str(round(i/Ni*100)),'%'])
-        end
-        for j = 1:Nj
-            if  length(sizes) == 3
-                inter_temp = squeeze(inter(j,i,:));                
-            else 
-                inter_temp = inter;
-            end
-            inter_temp = inter_temp.*exp(-(sigma*(l)*res).^2); % appodization
+    dl = l(2)-l(1);   
 
-            [S(j,i,:),~,nub] = mpfft(inter_temp,l); % FFT
-        end
+    % On reshape pour gagner en temps de calcul    
+    inter_reshaped = reshape(inter,Ni*Nj,Nk);
+    S_reshaped = zeros(Ni*Nj,(Nk-1)/2);
+    
+    % execution de la fft
+    for j = 1:Nj*Ni    
+        [S_reshaped(j,:),~,nub] = mpfft(inter_reshaped(j,:).*exp(-(sigma*(l)*res).^2)',l); % FFT
     end
+    
     disp('Calcul fft terminé')
     
     if nargout == 3
-        inter_apo = inter_temp.*exp(-(sigma*(l)*res).^2);
+        inter_apo = inter_reshaped(Ni*Nj,:)'.*exp(-(sigma*(l)*res).^2);
     end
+    S = reshape(S_reshaped,Nj,Ni,size(S_reshaped,2));
 end
 
