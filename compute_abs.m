3e17e887019c91dbb8d776a751791939d1c1f4f9
Stéphane Chevalier <stephane.chevalier@u-bordeaux.fr>
2020-08-05 15:47:23 +0200

    Nouvelle version de la fonction de calcul de l'absorbance

--- compute_abs.m
+++ compute_abs.m
@@ -1,13 +1,20 @@
-function Abs = compute_abs(S,path_bkg)
+function Abs = compute_abs(S,path_bkg,calibrated,TI)
 %Abs = compute_abs(S,path_bkg)
 %   S : 3D matrix of a IR spectrum (sample)
 % path_back : path to the IR spectrum of the background
+% calibrated for calibrated background
+% TI : integration time used for the sample
 
 disp('Computing the absorbance matrix');
 
 
-data_bkg = load([path_bkg,'pt/data.mat']);
-S_bkg = data_bkg.Spectre;
+if calibrated == 0
+    data_bkg = load([path_bkg,'pt/data.mat']);
+    S_bkg = data_bkg.Spectre;
+else
+    data_bkg = struct2array(load(path_bkg));
+    S_bkg = data_bkg*TI;    
+end
 
 for i = 1:size(S_bkg,3)
     Abs(:,:,i) = -log10(imdivide(S(:,:,i),S_bkg(:,:,i)));
